080c3a235c70c5311b3bc82625a550fffd205c8b
evgeniy <vois.evgenii@gmail.com>
2020-12-25 17:30:36 +0300

    Footer Component

--- frontend/src/ui/organisms/Footer/index.tsx
+++ frontend/src/ui/organisms/Footer/index.tsx
@@ -1,1 +1,20 @@
-export const Footer = () => <h1>fgdfgdfgf</h1>;
+import { ButtonPrimary } from '@ui/atoms/button_primary';
+import logo from '@images/logo.svg';
+import styles from './Footer.module.scss';
+
+export const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.wrapper}>
+      <div className={styles.flex1}>
+        <div>
+          <img src={logo} alt="" />
+          <span>Bookshop</span>
+        </div>
+      </div>
+      <div className={styles.flex2}>
+        <ButtonPrimary buttonText="login" />
+        <ButtonPrimary buttonText="Sign up" />
+      </div>
+    </div>
+  </footer>
+);

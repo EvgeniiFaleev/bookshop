9cbe880f3267304c179d3d76d624970f54b76b20
evgeniy <vois.evgenii@gmail.com>
2021-02-01 20:00:22 +0300

    markup for account icon nav

--- frontend/src/ui/organisms/Header/index.tsx
+++ frontend/src/ui/organisms/Header/index.tsx
@@ -5,26 +5,43 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 
+import { FC, useState } from 'react';
 import styles from './Header.module.scss';
-import {FC} from "react";
 
-export const Header:FC = ({children}) => (
-  <header className={styles.header}>
-    <div className={styles.wrapper}>
-      <Link to="/" className={styles.flex_1}>
-        <img className={styles.logo} src={logo} alt="logo" />
-        <h1 className={styles.title}>Bookshop</h1>
-      </Link>
-      <div className={styles.flex_2}>
-        <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
-        <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
-        <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
-        <p className={styles.browse}>Find a bookstore browse</p>
-        <Link className={styles.basket} to="/cart"><img src={basket} alt="basket" />
-          {children}</Link>
-        <Link className={styles.user} to="/user"><img src={user} alt="user" /></Link>
-        <div />
+export const Header:FC = ({ children }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <Link to="/" className={styles.flex_1}>
+          <img className={styles.logo} src={logo} alt="logo" />
+          <h1 className={styles.title}>Bookshop</h1>
+        </Link>
+        <div className={styles.flex_2}>
+          <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+          <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+          <p className={styles.browse}>Find a bookstore browse</p>
+          <Link className={styles.basket} to="/cart">
+            <img src={basket} alt="basket" />
+            {children}
+          </Link>
+          <div className={styles.user_wrapper} onClick={()=> setIsShown(!isShown)}>
+
+              <img
+                src={user}
+                alt="user"
+              />
+            <div className={`${styles.account_nav} ${isShown ? styles.active : ''}`}>
+              <Link to="/account">My account</Link>
+              <Link to="/account/wishlist">My wishlist</Link>
+              <div className={styles.arrow_up} />
+            </div>
+          </div>
+          <div />
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};

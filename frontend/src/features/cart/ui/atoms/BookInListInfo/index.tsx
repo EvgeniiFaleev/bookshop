4459c45fba574da08b45a6c0e51b73d58f0cda40
evgeniy <vois.evgenii@gmail.com>
2021-02-03 16:32:16 +0300

    WishLsit markUp is ready

--- frontend/src/features/cart/ui/atoms/BookInListInfo/index.tsx
+++ frontend/src/features/cart/ui/atoms/BookInListInfo/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { FC } from 'react';
-import styles from './BookCartInfo.module.scss';
+import styles from './BookInListInfo.module.scss';
 
 interface IBookInListInfo {
   picture: string,
@@ -10,12 +10,16 @@ interface IBookInListInfo {
   price? : number
 }
 export const BookInListInfo: FC<IBookInListInfo> = ({
-  picture, title, author, id, price
+  picture, title, author, id, price,
 }) => (
   <div className={styles.bookInfo}>
-    <Link to={`/book/${id}`}><img src={picture} alt="" /></Link>
+    <Link className={styles.picture_link} to={`/book/${id}`}><img src={picture} alt="" /></Link>
     <div className={styles.info}>
-      <p className={styles.title}>{title}</p>
+      <p className={styles.title}>
+        <Link to={`/book/${id}`}>
+          {title}
+        </Link>
+      </p>
       <p className={styles.author}>{author}</p>
       {price && <p className={styles.price}>{price}</p>}
     </div>
--- frontend/src/features/user/ui/organisms/WishList/index.tsx
+++ frontend/src/features/user/ui/organisms/WishList/index.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import { BookInListInfo } from '@cart';
 import { ButtonSecondary, ButtonPrimary } from '@ui';
 
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './WishList.module.scss';
 
 interface IWishListProps {
@@ -20,14 +22,22 @@ export const WishList: FC<IWishListProps> = ({ books }) => {
         id={item._id}
       />
       <div className={styles.buttons}>
-        <ButtonPrimary type="button" />
-        <ButtonSecondary type="button" />
+        <ButtonPrimary type="button">
+          <FontAwesomeIcon icon={faShoppingBasket} />
+          Add to Cart
+        </ButtonPrimary>
+        <ButtonSecondary type="button">Delete</ButtonSecondary>
       </div>
     </div>
   ));
   return (
     <div className={styles.wrapper}>
+      <h1 className={styles.head}>My Wishlist</h1>
+
       {booksElements}
+      <div className={styles.home_button}>
+        <ButtonPrimary path="/" type="link">Continue Shopping</ButtonPrimary>
+      </div>
     </div>
   );
 };
